41154e27149b6ca7aa0dce8bde10b5ce294c7c69
liorbarak <lior.barak1@mail.huji.ac.il>
2014-06-10 00:45:22 +0300

    first test

---
+++ testing.java
@@ -0,0 +1,18 @@
+
+public class testing {
+
+	
+	
+	public enum vars {INT , STUFF, CHAR };
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		vars a = vars.INT;
+		
+	}
+
+}
---
+++ testing2.java
@@ -0,0 +1,15 @@
+import testing.vars;
+
+
+
+
+public class testing2 {
+
+vars a = vars.CHAR;
+
+//sdkjvhskdvnlsdv
+//sdvnksdkvnsdklvndsv
+//sdvkndskvbsdkvbsdv
+//sdvkbsdjvbjdsvbjsdv
+//sdvjbdvjbdsvb
+}
