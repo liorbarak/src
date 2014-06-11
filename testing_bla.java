44321dbf0eb490962867d1de80d643569d21e5f0
liorbarak <lior.barak1@mail.huji.ac.il>
2014-06-11 14:24:08 +0300

    test change

---
+++ testing_bla.java
@@ -0,0 +1,18 @@
+
+public class testing_bla {
+
+	
+	
+	public enum vars {INT , STUFF, CHAR };
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		vars a = vars.INT;
+		
+	}
+
+}
