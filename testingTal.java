2ef49e04a5e8ff6f7dc09f0f3bf39e6b4b7596c8
liorbarak <lior.barak1@mail.huji.ac.il>
2014-06-12 12:13:39 +0300

    check merge test

--- testingTal.java
+++ testingTal.java
@@ -2,15 +2,23 @@
 public class testingTal {
 
 	enum types{ABC, DEF, GHI};
-	
+
 	public static void main(types type) {
-		
-		
+
+
+
 		switch(type) {
-		
+
 		case ABC: return; 
-			
+
 		}
-		
+
+
+	}
+	
+	//lior added comment
+	public int liorisgreat(){
+		return 0;
+		//
 	}
 }
