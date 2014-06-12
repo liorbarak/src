fd6f629414b1df4354f7b8693e25494e6e645e4d
liorbarak <lior.barak1@mail.huji.ac.il>
2014-06-12 12:37:55 +0300

    added type types

--- oop/ex7/type/Char.java
+++ oop/ex7/type/Char.java
@@ -1,18 +1,11 @@
 package oop.ex7.type;
 
-
-/**
- * This class represents the int variable type. 
- * @author taldovrat
- *
- */
-public class Int implements Type {
-
+public class Char implements Type {
 
 	@Override
 	public boolean checkExpression(String check) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 }
--- oop/ex7/type/Type.java
+++ oop/ex7/type/Type.java
@@ -11,32 +11,38 @@ package oop.ex7.type;
 public interface Type {
 	
 	
-	@Override
-	/**
-	 * This method overrides the toString of Object class. 
-	 * @return The string representation of the Type: "String", "int", "char", 
-	 * exc.
-	 */
-	public String toString();
+//	@Override
+//	/**
+//	 * This method overrides the toString of Object class. 
+//	 * @return The string representation of the Type: "String", "int", "char", 
+//	 * exc.
+//	 */
+//	public String toString();
+//	
+//	/**
+//	 * This method checks if the code contains a correct variable declaration.
+//	 * for example, if the code is: "int a;", the method will check if the word
+//	 * int is correct, and in this case the method should do nothing. for
+//	 * example, if the code is: "inti a;", the varibale declaration is 
+//	 * incorrect (inti instead of int) and the method should throw an Exception
+//	 */
+//	public void checkType(String strToCheck) throws BadVarDeclarationException;
+//	
+//	/**
+//	 * This method checks if the input expression in the code matches the 
+//	 * variable type. For example, if the code has the line: "int a = 3;", the
+//	 * input expression is the number "3", and in this case the method should
+//	 * do nothing. if the code contains the line: "int a = "str";, the method
+//	 * should throw a BadInputException and inform the user about the error. 
+//	 * @throw BadInputException
+//	 * @return
+//	 */
 	
 	/**
-	 * This method checks if the code contains a correct variable declaration.
-	 * for example, if the code is: "int a;", the method will check if the word
-	 * int is correct, and in this case the method should do nothing. for
-	 * example, if the code is: "inti a;", the varibale declaration is 
-	 * incorrect (inti instead of int) and the method should throw an Exception
-	 */
-	public void checkType(String strToCheck) throws BadVarDeclarationException;
-	
-	/**
-	 * This method checks if the input expression in the code matches the 
-	 * variable type. For example, if the code has the line: "int a = 3;", the
-	 * input expression is the number "3", and in this case the method should
-	 * do nothing. if the code contains the line: "int a = "str";, the method
-	 * should throw a BadInputException and inform the user about the error. 
-	 * @throw BadInputException
+	 * checks if a String expression is of the correct type for a specific type
+	 * @param check
 	 * @return
 	 */
-	public void checkExpression() throws BadInputException;
+	public boolean checkExpression(String check);
 	
 }
