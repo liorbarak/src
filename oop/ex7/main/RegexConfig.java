b897a8ba476f101af8f87d6c648aa0764e4cb9d4
liorbarak <lior.barak1@mail.huji.ac.il>
2014-06-14 00:12:26 +0300

    try again

--- oop/ex7/main/RegexConfig.java
+++ oop/ex7/main/RegexConfig.java
@@ -9,23 +9,23 @@ package oop.ex7.main;
  */
 public class RegexConfig {
 
-//	public static final String INT = "int";
-//	public static final String DOUBLE = "double";
-//	public static final String STRING = "String";
-//	public static final String BOOLEAN = "boolean";
-//	public static final String CHAR = "char";
-//	public static final String VOID = "void";
-	
-	
+	//	public static final String INT = "int";
+	//	public static final String DOUBLE = "double";
+	//	public static final String STRING = "String";
+	//	public static final String BOOLEAN = "boolean";
+	//	public static final String CHAR = "char";
+	//	public static final String VOID = "void";
+
+
 	//not necessarily needed. 
 	public enum lineType {
 		DECLARATION (validTypes+"[ ]+"+GENERAL_NAME+"[ ]*;[ ]*"),
 		ASSIGNMENT (GENERAL_NAME+"[ ]*=[ ]*"+VALID_EXP+"[ ]*;[ ]*"), 
 		BOTH (),
 		RETURN ();
-		
+
 		private static final String regex;
-		
+
 		lineType(String regex) {
 			this.regex = regex;
 		}
@@ -33,47 +33,47 @@ public class RegexConfig {
 			return regex;
 		}
 	}
-	
-	
-	
-	public static final String validTypes = "( )*(int|double|String|char|boolean)( )*";
+
+
+
+	public static final String VALID_TYPES = "( )*(int|double|String|char|boolean)( )*";
 	public static final String GENERAL_NAME = "[_]?[^ ]+( )*";
 	public static final String UNINITIALIZED = ";";
-	
-	
-	
+
+
+
 	public static final String TYPE_INT = "( )*int( )*";
 	public static final String INPUT_INT = "=( )*(-)?[\\d]+( )*;";
-	
+
 	public static final String TYPE_DOUBLE = "( )*double( )*";
 	public static final String INPUT_DOUBLE = "("+INPUT_INT+"|=( )*(-)?[\\d]+.[\\d]+( )*;)";
-	
+
 	public static final String TYPE_STRING = "( )*String( )*";
 	public static final String INPUT_STRING = "=( )*\"[^\"]*\"( )*;";
-	
+
 	public static final String TYPE_BOOLEAN = "( )*boolean( )*";
 	public static final String INPUT_BOOLEAN = "=( )*(true|false)( )*;";
-	
+
 	public static final String TYPE_CHAR = "( )*char( )*";
 	public static final String INPUT_CHAR = "=( )*'[^']?'( )*;";
-	
+
 	public static final String SOME_TYPE_VALUE = "("+INPUT_INT+"|"+
-													INPUT_DOUBLE+"|"+
-													INPUT_CHAR+"|"+
-													INPUT_STRING+"|"+
-													INPUT_BOOLEAN+")";
-	
+			INPUT_DOUBLE+"|"+
+			INPUT_CHAR+"|"+
+			INPUT_STRING+"|"+
+			INPUT_BOOLEAN+")";
+
 	public static final String VALID_METHOD_CALL = GENERAL_NAME+"((("+GENERAL_NAME+")?|(("+GENERAL_NAME+"),)+))";
 
-	
+
 	public static final String VALID_EXP = "(GENERAL_NAME|SOME_TYPE_VALUE|VALID_METHOD_CALL)";
-	
+
 	public static final String INT_REGEX_EXP = TYPE_INT+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_INT+")";
 	public static final String DOUBLE_REGEX_EXP = TYPE_DOUBLE+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_DOUBLE+")";
 	public static final String STRING_REGEX_EXP = TYPE_STRING+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_STRING+")";
 	public static final String BOOLEAN_REGEX_EXP = TYPE_BOOLEAN+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_BOOLEAN+")";
 	public static final String CHAR_REGEX_EXP = TYPE_CHAR+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_CHAR+")";
-		
+
 	public static final String INT = "int";
 	public static final String DOUBLE = "double";
 	public static final String STRING = "String";
@@ -81,5 +81,14 @@ public class RegexConfig {
 	public static final String CHAR = "char";
 	public static final String VOID = "void";
 
-	
+
+	public static final String VALID_IF_CALL ="( )*if( )*[(].+[)]( )*[{]( )*";
+	public static final String VALID_WHILE_CALL="( )*while( )*[(].+[)]( )*[{]( )*";
+	public static final String TYPE_PLUS_VAR = "( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*"; 
+	public static final String VALID_METHOD_DECLARE="( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*[(]"+(TYPE_PLUS_VAR+",")+"*"+TYPE_PLUS_VAR+"[)]( )*[{]( )*";
+
+//	public static final String
+//	public static final String
+
+
 }
--- testing_LIOR.java
+++ testing_LIOR.java
@@ -1,24 +1,35 @@
 
 public class testing_LIOR {
 
+
+
 	
+	public static final String VALID_IF_CALL ="( )*if( )*[(].+[)]( )*[{]";
+	
+	
+	public static final String VALID_TYPES = "( )*(int|double|String|char|boolean)( )*";
+	public static final String GENERAL_NAME = "[_]?[^ ]+( )*";
+	
+	
+	public static final String TYPE_PLUS_VAR = "( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*"; 
+	public static final String VALID_METHOD_DECLARE="( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*[(]"+(TYPE_PLUS_VAR+",")+"*"+TYPE_PLUS_VAR+"[)]( )*[{]( )*";
 
 	public enum vars {INT , STUFF, CHAR };
 	/**
 	 * @param args
 	 */
+	
+	
 	public static void main(String[] args) {
 
-		DifferentTypes[] abc=DifferentTypes.values();
 		
-		for(DifferentTypes i:abc){
-			System.out.println(i);
-		}
+		
+		//String test="int abc (int a, int b, char zxcv) {";
+		String test="char a";
+		System.out.println(test.matches(TYPE_PLUS_VAR));
+
+	}
 
 }
-	
-	public int bla(int a){
-		a=6;
-		return 8;
-	}
-}
+
+
