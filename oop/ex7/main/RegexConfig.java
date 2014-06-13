941c435b63bfb3bf50fe6fc0a4dec60f68fb137a
taldo <taldovrat@Tals-MacBook-Pro.local>
2014-06-14 00:36:24 +0300

    changed regexon regex config

--- oop/ex7/main/RegexConfig.java
+++ oop/ex7/main/RegexConfig.java
@@ -18,26 +18,26 @@ public class RegexConfig {
 
 
 	//not necessarily needed. 
-	public enum lineType {
-		DECLARATION (validTypes+"[ ]+"+GENERAL_NAME+"[ ]*;[ ]*"),
-		ASSIGNMENT (GENERAL_NAME+"[ ]*=[ ]*"+VALID_EXP+"[ ]*;[ ]*"), 
-		BOTH (),
-		RETURN ();
-
-		private static final String regex;
-
-		lineType(String regex) {
-			this.regex = regex;
-		}
-		public String getRegex() {
-			return regex;
-		}
-	}
+//	public enum lineType {
+//		DECLARATION (validTypes+"[ ]+"+GENERAL_NAME+"[ ]*;[ ]*"),
+//		ASSIGNMENT (GENERAL_NAME+"[ ]*=[ ]*"+VALID_EXP+"[ ]*;[ ]*"), 
+//		BOTH (),
+//		RETURN ();
+//
+//		private static final String regex;
+//
+//		lineType(String regex) {
+//			this.regex = regex;
+//		}
+//		public String getRegex() {
+//			return regex;
+//		}
+//	}
 
 
 
 	public static final String VALID_TYPES = "( )*(int|double|String|char|boolean)( )*";
-	public static final String GENERAL_NAME = "[_]?[^ ]+( )*";
+	public static final String GENERAL_NAME = "( )*([_][^ ]+|[^\\d_][^ ]*)( )*";
 	public static final String UNINITIALIZED = ";";
 
 
@@ -63,10 +63,10 @@ public class RegexConfig {
 			INPUT_STRING+"|"+
 			INPUT_BOOLEAN+")";
 
-	public static final String VALID_METHOD_CALL = GENERAL_NAME+"((("+GENERAL_NAME+")?|(("+GENERAL_NAME+"),)+))";
+	public static final String METHOD_CALL = "( )*"+GENERAL_NAME+"( )*\\(( )*[\\d]*[\\D]*( )*\\)( )*;?";
 
 
-	public static final String VALID_EXP = "(GENERAL_NAME|SOME_TYPE_VALUE|VALID_METHOD_CALL)";
+	public static final String VALID_EXP = "("+GENERAL_NAME+"|"+SOME_TYPE_VALUE+"|"+METHOD_CALL+")";
 
 	public static final String INT_REGEX_EXP = TYPE_INT+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_INT+")";
 	public static final String DOUBLE_REGEX_EXP = TYPE_DOUBLE+GENERAL_NAME+"("+UNINITIALIZED+"|"+INPUT_DOUBLE+")";
@@ -82,8 +82,8 @@ public class RegexConfig {
 	public static final String VOID = "void";
 
 
-	public static final String VALID_IF_CALL ="( )*if( )*[(].+[)]( )*[{]( )*";
-	public static final String VALID_WHILE_CALL="( )*while( )*[(].+[)]( )*[{]( )*";
+	public static final String VALID_IF_CALL ="( )*if( )*[(]( )*"+VALID_EXP+"( )*[)]( )*[{]( )*";
+	public static final String VALID_WHILE_CALL="( )*while( )*[(]( )*"+VALID_EXP+"( )*[)]( )*[{]( )*";
 	public static final String TYPE_PLUS_VAR = "( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*"; 
 	public static final String VALID_METHOD_DECLARE="( )*"+VALID_TYPES+"( )+"+GENERAL_NAME+"( )*[(]"+(TYPE_PLUS_VAR+",")+"*"+TYPE_PLUS_VAR+"[)]( )*[{]( )*";
 
@@ -91,4 +91,10 @@ public class RegexConfig {
 //	public static final String
 
 
+	public static void main(String[] args) {
+//		String test = "aff (as);";
+//		String test = "int  (){";
+		
+		System.out.println(test.matches(VALID_WHILE_CALL));
+	}
 }
