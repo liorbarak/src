38d745ab7292e19c564f4abd40a39979d066dc01
taldo <taldovrat@Tals-MacBook-Pro.local>
2014-06-16 21:45:18 +0300

    added trim

--- oop/ex7/scope/Scope.java
+++ oop/ex7/scope/Scope.java
@@ -398,7 +398,7 @@ public Variable varExist(String nameOfVar) {
 	Scope tempScope = this; 
 
 	for (Variable varOfScope:tempScope.getVariables()) {
-		if (varOfScope.getName().equals(nameOfVar)) {
+		if (varOfScope.getName().equals(nameOfVar.trim())) {
 			return varOfScope;
 		}
 	}
@@ -488,7 +488,7 @@ private void methodInput (ArrayList<String> lines,int start, int finish) throws 
 		MethodScope method=(MethodScope) i;
 
 		if (method.getNameOfMethod().equals(methodName)){
-			throw new DoubleMethodException(finish, lines.get(start));
+			throw new DoubleMethodException(lines.get(start));
 
 		}
 	}
